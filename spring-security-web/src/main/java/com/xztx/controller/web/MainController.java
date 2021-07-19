92ae73a7c77b97ec80596448060f554b2bfbd8b3
xztx <15767187537@163.com>
2021-07-19 21:55:27 +0800

    增加获取用户信息的三种方式

--- spring-security-web/src/main/java/com/xztx/controller/web/MainController.java
+++ spring-security-web/src/main/java/com/xztx/controller/web/MainController.java
@@ -1,7 +1,15 @@
 package com.xztx.controller.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.SpringSecurityMessageSource;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Security;
 
 /**
  * @Auther: 梦学谷 www.mengxuegu.com
@@ -12,6 +20,43 @@ public class MainController {
     @RequestMapping({"/index", "/", ""})
     public String index() {
         return "index";// resources/templates/index.html
+    }
+
+    /***
+     * 获取用户信息的第一种方式
+     */
+    @RequestMapping("/user/info1")
+    @ResponseBody
+    public UserDetails getUserInfo() {
+        UserDetails userDetails = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        if(principal instanceof UserDetails) {
+            userDetails = (UserDetails) principal;
+        }
+        return userDetails;
+    }
+
+    /***
+     * 获取用户信息的方式2
+     * @param authentication
+     * @return
+     */
+    @RequestMapping("/user/info2")
+    @ResponseBody
+    public Object getUserInfo2(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+
+    /***
+     * 获取用户信息的方式3
+     * @param userDetails
+     * @return
+     */
+    @RequestMapping("/user/info3")
+    @ResponseBody
+    public Object getUserInfo3(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails;
     }
 
 }
